d947e68b41561e946e37f8b5a595e1549d4b3cff
Faddal Ibrahim <faddalibrahim@gmail.com>
2023-11-07 11:06:40 +0000

    feat: create dummy dropdown filter

---
+++ src/components/DropdownFilter/DropdownFilter.jsx
@@ -0,0 +1,21 @@
+const DropdownFilter = () => {
+  return (
+    <div>
+      <select
+        id="dropdown-filter"
+        className="outline-none border-none py-3 px-2 rounded"
+      >
+        <option value="a">a</option>
+        <option value="b">b</option>
+        <option value="c">c</option>
+        <option value="d">d</option>
+        <option value="e">e</option>
+        <option value="f">f</option>
+        <option value="g">g</option>
+        <option value="h">h</option>
+      </select>
+    </div>
+  );
+};
+
+export default DropdownFilter;
