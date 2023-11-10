db7ae1c67ff886f6d889f1111b2fc3c8a90e3461
Faddal Ibrahim <faddalibrahim@gmail.com>
2023-11-10 16:34:31 +0000

    tests: write e2e tests for dashboard cards expansion

---
+++ cypress/e2e/expand_dashboards.cy.js
@@ -0,0 +1,30 @@
+describe("Expand Cases Malaria Dashboard", () => {
+  it("expands Cases Malaria card", () => {
+    cy.visit("http://localhost:5173");
+    cy.contains("Cases Malaria").click();
+  });
+});
+
+describe("Expand Delivery Dashboard", () => {
+  it("expands Delivery card", () => {
+    cy.visit("http://localhost:5173");
+    // cy.get(".dashboard-card").first().click();
+    cy.contains("Delivery").click();
+  });
+});
+
+describe("Expand Disease Surveillance Dashboard", () => {
+  it("expands Disease Surveillance card", () => {
+    cy.visit("http://localhost:5173");
+    // cy.get(".dashboard-card").first().click();
+    cy.contains("Disease Surveillance").click();
+  });
+});
+
+describe("Expand Immunization Dashboard", () => {
+  it("expands Immunization card", () => {
+    cy.visit("http://localhost:5173");
+    // cy.get(".dashboard-card").first().click();
+    cy.contains("Immunization").click();
+  });
+});
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,5 @@
+describe("Load Application", () => {
+  it("loads the dashboard app", () => {
+    cy.visit("http://localhost:5173");
+  });
+});
